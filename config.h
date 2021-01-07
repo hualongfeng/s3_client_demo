ac4c4287b9065e39eadf8fa3766ab098453bf1ed
Hualong Feng <hualong.feng@intel.com>
2021-01-07 11:04:12 +0800

    move httpRequest to request.h

---
+++ config.h
@@ -0,0 +1,7 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+const char* access_key = "0555b35654ad1656d804";
+const char* secret_key = "h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q==";
+
+#endif // _CONFIG_H_
